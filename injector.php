086ef8298db47e42a68a84094a4e7e2e02741ca3
Henri Bergius <henri.bergius@iki.fi>
2010-12-02 10:48:53 +0200

    Inject the template

---
+++ injector.php
@@ -0,0 +1,10 @@
+<?php
+class com_meego_website_injector
+{
+    public function inject_template(midgardmvc_core_request $request)
+    {
+        // We inject the template to provide MeeGo styling
+        $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('com_meego_website'), true);
+    }
+}
+?>
