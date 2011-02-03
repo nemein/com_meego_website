1b0b61283b366b4255a4935694899ef1cae03036
Niels Breet <nbreet@gmail.com>
2011-02-03 23:13:26 +0000

    More changes

--- controllers/avatar.php
+++ controllers/avatar.php
@@ -10,27 +10,29 @@ class com_meego_website_controllers_avatar extends midgardmvc_helper_attachments
     {
         $ar = array('login' => $args['username'], 'authtype' => 'LDAP');
 
-        $users = new midgard_user($ar);
-        if ($users[0])
+        $user = new midgard_user($ar);
+        if ($user)
         {
-            $attachments = $user[0]->list_attachments();
+            $attachments = $user->get_person()->list_attachments();
             //Check if attachement exists
             if (count($attachments) == 0)
             {
                 //fetch avatar from meego.com
-                $employeenumber = $user[0]->get_person()->get_parameter('midgardmvc_core_services_authentication_ldap', 'employeenumber');
-                $attachment = $users[0]->create_attachment('meego:avatar', 'meego:avatar', 'image/png');
-                midgardmvc_helper_attachmentserver::copy_file_to_attachment('http://meego.com/sites/all/files/imagecache/user_pics/user_pics/picture-' . $employeenumber . '.png', $attachment);
-                $attachments[0] = $attachment;
+                $employeenumber = $user->get_person()->get_parameter('midgardmvc_core_services_authentication_ldap', 'employeenumber');
+                $attachment = $user->get_person()->create_attachment('meego:avatar', 'meego:avatar', 'image/png');
+                //Does not work through proxy as is.
+//                midgardmvc_helper_attachmentserver_helpers::copy_file_to_attachment('http://meego.com/sites/all/files/imagecache/user_pics/user_pics/picture-' . $employeenumber . '.png', $attachment);
+//                $attachments[0] = $attachment;
             }
+
             if (count($attachments) > 0)
             {
                 //serve attachment
-                $this->serve_attachement($attachments[0]);
-                return;
+                $this->serve_attachment($attachments[0]);
             }
         }
         //redirect to default avatar
-         midgardmvc_core::get_instance()->head->relocate('http://meego.com/sites/all/themes/meego/images/peep_skate.png');
+        midgardmvc_core::get_instance()->head->relocate('http://meego.com/sites/all/themes/meego/images/peep_skate.png');
     }
 }
+?>
