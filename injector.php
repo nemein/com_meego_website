34c9180514fdd1431412aec86ae07a2da3a69dae
Niels Breet <nbreet@gmail.com>
2011-02-03 21:32:13 +0000

    Add avatar serving and fetching from meego.com

--- injector.php
+++ injector.php
@@ -1,7 +1,7 @@
 <?php
 class com_meego_website_injector
 {
-    public function inject_template(midgardmvc_core_request $request)
+    public function inject_process(midgardmvc_core_request $request)
     {
         // We inject the template to provide MeeGo styling
         $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('com_meego_website'), true);
