cef37ab9d5f9151b668d29c2f5fa06601ff94186
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-03-14 15:26:08 +0200

    if user is admin then provide a link to category management ui

--- injector.php
+++ injector.php
@@ -1,10 +1,37 @@
 <?php
 class com_meego_website_injector
 {
+    var $mvc = null;
+    var $request = null;
+
+    public function __construct()
+    {
+        $this->mvc = midgardmvc_core::get_instance();
+    }
+
     public function inject_process(midgardmvc_core_request $request)
     {
         // We inject the template to provide MeeGo styling
-        $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('com_meego_website'), true);
+        $request->add_component_to_chain($this->mvc->component->get('com_meego_website'), true);
+        $this->request = $request;
+    }
+
+    /**
+     * Some template hack
+     */
+    public function inject_template(midgardmvc_core_request $request)
+    {
+        $route = $request->get_route();
+
+        $request->set_data_item('admin', false);
+
+        if ($this->mvc->authentication->is_user())
+        {
+            if ($this->mvc->authentication->get_user()->is_admin())
+            {
+                $request->set_data_item('admin', true);
+            }
+        }
     }
 }
 ?>
